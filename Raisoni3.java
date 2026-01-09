f83d76a3b878775c5997364d7e51748e0cf87cd7
Yash Selokar <yash20selokar22@gmail.com>
2026-01-09 23:40:41 +0530

    Add files via upload

---
+++ Raisoni3.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Raisoni3 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Enter the value of a: ");
+        int a= sc.nextInt();//for integer input
+        System.out.println("Value of a is: "+a);
+    
+    }
+}
