09fbe47ab377fee1cb92e2c0e14eea7f3b467bac
Yash Selokar <yash20selokar22@gmail.com>
2026-01-11 10:23:11 +0530

    Add files via upload

---
+++ Raisoni15.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class Raisoni15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the any number: ");
+        int num = sc.nextInt();
+
+        int b=num;
+        int r;
+        int sum=0;
+
+        while(num>0){
+            r=num%10;
+            r=r*r*r;
+            sum=sum*10+r;
+            num=num/10;
+        }
+        if(sum==b){
+            System.out.println("The number is ARMSTRONG");
+        }
+        else{
+            System.out.println("The number is not ARMSTRONG");
+        }
+    }
+}
